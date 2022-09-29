f7f24ac00a6ec7c75b6fdacf749e8bc7914ed29e
leonard-porteria <porterialeonard@gmail.com>
2022-09-29 19:28:31 +0800

    append todo to users using schema

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const { ObjectId } = require("mongodb");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
@@ -19,13 +20,12 @@ app.get("/", (req, res) => {
   res.sendFile("./src/index.html", { root: __dirname });
 });
 
-// default endpoint
-app.get("/todo-list", (req, res) => {
+// DEFAULT ENDPOINT
+app.get("/todos", (req, res) => {
   let users = [];
 
-  db.collection("users")
+  db.collection("todos")
     .find()
-    .sort({ author: 1 })
     .forEach((user) => users.push(user))
     .then(() => {
       res.status(200).json(users);
@@ -35,11 +35,77 @@ app.get("/todo-list", (req, res) => {
     });
 });
 
-// get userId endpoint
+// GET BY ID
+app.get("/todos/:id", (req, res) => {
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(500).json({ error: "object id invalid" });
+    return;
+  }
 
-// delete userId endpoint
+  db.collection("todos")
+    .findOne({ _id: ObjectId(req.params.id) })
+    .then((doc) => {
+      res.status(200).json(doc);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err });
+    });
+});
 
-// update userId endpoint
+// POST
+app.post("/todos", (req, res) => {
+  const todo = req.body;
+
+  db.collection("todos")
+    .insertOne(todo)
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err });
+    });
+});
+
+// DELETE BY ID
+app.delete("/todos/:id", (req, res) => {
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(500).json({ error: "object id invalid" });
+    return;
+  }
+
+  db.collection("todos")
+    .deleteOne({ _id: ObjectId(req.params.id) })
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err });
+    });
+});
+
+// UPDATE BY ID
+app.patch("/todos/:id", (req, res) => {
+  // process
+  // > remove old todo object by id
+  // > create new todo schema
+  // > add to the collection
+
+  const updates = req.body;
+
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(500).json({ error: "object id invalid" });
+    return;
+  }
+
+  db.collection("todos")
+    .updateOne({ _id: ObjectId(req.params.id) }, { $set: updates })
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err });
+    });
+});
 
 // HELPER FUNCTIONS
 async function createUser() {
@@ -50,13 +116,33 @@ async function createUser() {
       username: "userUsername2",
       createdAt: Date.now(),
       updatedAt: Date.now(),
-      // body: {
-      //   todo: "to do stuff2",
-      //   label: "important2",
-      //   description: "to do this stuff2",
-      //   createdAt: Date.now(),
-      //   updatedAt: Date.now(),
-      // },
+      body: {
+        todo: "to do stuff2",
+        label: "important2",
+        description: "to do this stuff2",
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+      },
+    });
+    console.log(user);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function addTodo() {
+  try {
+    const user = await Users.create({
+      email: "test2@email.com",
+      password: "password",
+      username: "userUsername2",
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+      body: {
+        todo: "to do stuff2",
+        label: "important2",
+        description: "to do this stuff2",
+      },
     });
     console.log(user);
   } catch (err) {
